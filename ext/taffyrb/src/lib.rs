25976d7228d7dfd5f0af78fd0f73df1f3d0dcd82
Brian Field <brian@madsalty.com>
2023-08-30 23:40:25 -0700

    Kinda works?

--- ext/taffyrb/src/lib.rs
+++ ext/taffyrb/src/lib.rs
@@ -1,12 +1,206 @@
-use magnus::{define_module, function, prelude::*, Error};
+use magnus::scan_args::{get_kwargs, scan_args};
+use magnus::{class, define_class, exception, function, method, prelude::*, Error, Value};
+use magnus::{Symbol, TryConvert};
+use taffy::prelude::*;
+use taffy::style::Dimension;
 
-fn hello(subject: String) -> String {
-    format!("Hello from Rust, {}!", subject)
+#[magnus::wrap(class = "Taffy", free_immediately, size)]
+struct TaffyRB(std::cell::RefCell<Taffy>);
+
+impl TaffyRB {
+    fn new() -> Self {
+        Self(std::cell::RefCell::new(Taffy::new()))
+    }
+
+    fn new_leaf(&self, style: &TaffyRBStyle) -> TaffyRBNode {
+        let mut inner = self.0.borrow_mut();
+        let node = inner.new_leaf(style.0.borrow().clone()).unwrap();
+        TaffyRBNode(node)
+    }
+
+    fn add_child(&self, parent: &TaffyRBNode, child: &TaffyRBNode) {
+        let mut inner = self.0.borrow_mut();
+        inner.add_child(parent.0, child.0).unwrap();
+    }
+
+    fn child_count(&self, parent: &TaffyRBNode) -> usize {
+        let inner = self.0.borrow();
+        inner.child_count(parent.0).unwrap()
+    }
+
+    fn compute_layout(&self, node: &TaffyRBNode) {
+        let inner = &self.0;
+        inner
+            .borrow_mut()
+            .compute_layout(node.0, Size::MAX_CONTENT)
+            .unwrap();
+        std::cell::Ref::map(inner.borrow(), |inner| {
+            taffy::debug::print_tree(inner, node.0);
+            inner
+        });
+    }
+
+    fn layout(&self, node: &TaffyRBNode) -> TaffyRBLayout {
+        let inner = self.0.borrow();
+        TaffyRBLayout(*inner.layout(node.0).unwrap())
+    }
+}
+
+#[magnus::wrap(class = "Taffy::Layout", free_immediately, size)]
+struct TaffyRBLayout(Layout);
+
+impl TaffyRBLayout {
+    fn size(&self) -> (f32, f32) {
+        (self.0.size.width, self.0.size.height)
+    }
+}
+
+#[magnus::wrap(class = "Taffy::Size", free_immediately, size)]
+struct TaffyRBSize(Size<Dimension>);
+
+impl TryConvert for TaffyRBSize {
+    fn try_convert(value: magnus::Value) -> Result<Self, Error> {
+        let value = magnus::RHash::from_value(value).unwrap();
+
+        let mut size = Size {
+            width: Dimension::Auto,
+            height: Dimension::Auto,
+        };
+
+        if let Ok(width) = value.fetch::<_, f32>(Symbol::new("width_pts")) {
+            size.width = points(width);
+        }
+
+        if let Ok(height) = value.fetch::<_, f32>(Symbol::new("height_pts")) {
+            size.height = points(height);
+        }
+
+        if let Ok(width) = value.fetch::<_, f32>(Symbol::new("width_pct")) {
+            size.width = percent(width);
+        }
+
+        if let Ok(height) = value.fetch::<_, f32>(Symbol::new("height_pct")) {
+            size.height = percent(height);
+        }
+
+        if value
+            .fetch::<_, bool>(Symbol::new("width_auto"))
+            .ok()
+            .is_some()
+        {
+            size.width = Dimension::Auto;
+        }
+
+        if value
+            .fetch::<_, bool>(Symbol::new("height_auto"))
+            .ok()
+            .is_some()
+        {
+            size.height = Dimension::Auto;
+        }
+
+        Ok(Self(size))
+    }
+}
+
+#[magnus::wrap(class = "Taffy::Style", free_immediately, size)]
+#[derive(Clone, Debug, Default)]
+struct TaffyRBStyle(std::cell::RefCell<Style>);
+
+impl TaffyRBStyle {
+    fn initialize(rb_self: magnus::typed_data::Obj<Self>, args: &[Value]) -> Result<(), Error> {
+        let args = scan_args::<(), (), (), (), _, ()>(args)?;
+        let kwargs = get_kwargs::<
+            _,
+            (),
+            (
+                Option<TaffyRBDisplay>,
+                Option<TaffyRBSize>,
+                Option<TaffyRBFlexDirection>,
+                Option<f32>,
+            ),
+            (),
+        >(
+            args.keywords,
+            &[],
+            &["display", "size", "flex_direction", "flex_grow"],
+        )?;
+
+        if let Some(display) = kwargs.optional.0 {
+            rb_self.0.borrow_mut().display = display.0;
+        }
+
+        if let Some(size) = kwargs.optional.1 {
+            rb_self.0.borrow_mut().size = size.0;
+        }
+
+        if let Some(flex_direction) = kwargs.optional.2 {
+            rb_self.0.borrow_mut().flex_direction = flex_direction.0;
+        }
+
+        if let Some(flex_grow) = kwargs.optional.3 {
+            rb_self.0.borrow_mut().flex_grow = flex_grow;
+        }
+
+        Ok(())
+    }
+}
+
+#[magnus::wrap(class = "Taffy::Node", free_immediately, size)]
+struct TaffyRBNode(Node);
+
+#[magnus::wrap(class = "Taffy::Display", free_immediately, size)]
+struct TaffyRBDisplay(Display);
+
+impl TryConvert for TaffyRBDisplay {
+    fn try_convert(value: magnus::Value) -> Result<Self, Error> {
+        let value = Symbol::from_value(value).unwrap().name()?;
+
+        let display = match value.into_owned().as_str() {
+            "flex" => Display::Flex,
+            "grid" => Display::Grid,
+            "none" => Display::None,
+            _ => return Err(Error::new(exception::arg_error(), "no good")),
+        };
+        Ok(Self(display))
+    }
+}
+
+#[magnus::wrap(class = "Taffy::FlexDirection", free_immediately, size)]
+struct TaffyRBFlexDirection(FlexDirection);
+
+impl TryConvert for TaffyRBFlexDirection {
+    fn try_convert(value: magnus::Value) -> Result<Self, Error> {
+        let value = Symbol::from_value(value).unwrap().name()?;
+
+        let direction = match value.into_owned().as_str() {
+            "row" => FlexDirection::Row,
+            "row-reverse" => FlexDirection::RowReverse,
+            "column" => FlexDirection::Column,
+            "column-reverse" => FlexDirection::ColumnReverse,
+            _ => return Err(Error::new(exception::arg_error(), "no good")),
+        };
+        Ok(Self(direction))
+    }
 }
 
 #[magnus::init]
 fn init() -> Result<(), Error> {
-    let module = define_module("Taffyrb")?;
-    module.define_singleton_method("hello", function!(hello, 1))?;
+    let klass = define_class("Taffy", class::object())?;
+    klass.define_singleton_method("new", function!(TaffyRB::new, 0))?;
+    klass.define_method("new_leaf", method!(TaffyRB::new_leaf, 1))?;
+    klass.define_method("child_count", method!(TaffyRB::child_count, 1))?;
+    klass.define_method("add_child", method!(TaffyRB::add_child, 2))?;
+    klass.define_method("compute_layout", method!(TaffyRB::compute_layout, 1))?;
+    klass.define_method("layout", method!(TaffyRB::layout, 1))?;
+
+    let style_klass = klass.define_class("Style", class::object())?;
+    style_klass.define_alloc_func::<TaffyRBStyle>();
+    style_klass.define_method("initialize", method!(TaffyRBStyle::initialize, -1))?;
+
+    klass.define_class("Node", class::object())?;
+    let layout_klass = klass.define_class("Layout", class::object())?;
+    layout_klass.define_method("size", method!(TaffyRBLayout::size, 0))?;
+
     Ok(())
 }
--- test/test_taffyrb.rb
+++ test/test_taffyrb.rb
@@ -8,6 +8,59 @@ class TestTaffyrb < Minitest::Test
   end
 
   def test_it_does_something_useful
-    assert false
+    taffy = Taffy.new
+    root_style = Taffy::Style.new(
+      display: :flex,
+      size: {
+        width_pts: 100,
+        height_pts: 100
+      }
+    )
+    root_node = taffy.new_leaf(root_style)
+    another_node = taffy.new_leaf(root_style)
+    grandchild_node = taffy.new_leaf(root_style)
+    taffy.add_child(root_node, grandchild_node)
+    taffy.add_child(root_node, another_node)
+    assert_equal 2, taffy.child_count(root_node)
+
+    taffy.compute_layout(root_node)
+    layout = taffy.layout(root_node)
+    assert_equal [100, 100], layout.size
+
+    layout = taffy.layout(another_node)
+    assert_equal [50, 100.00], layout.size
+  end
+
+  def test_readme
+    taffy = Taffy.new
+    header_node = taffy.new_leaf(
+      Taffy::Style.new(
+        size: {width_pts: 800, height_pts: 100}
+      )
+    )
+
+    body_node = taffy.new_leaf(
+      Taffy::Style.new(
+        size: {width_pts: 800, height_auto: true},
+        flex_grow: 1.0
+      )
+    )
+
+    root_node = taffy.new_leaf(
+      Taffy::Style.new(
+        size: {width_pts: 800, height_pts: 600},
+        flex_direction: :column,
+        display: :flex
+      )
+    )
+
+    taffy.add_child(root_node, header_node)
+    taffy.add_child(root_node, body_node)
+
+    taffy.compute_layout(root_node)
+
+    assert_equal(taffy.layout(root_node).size, [800, 600])
+    assert_equal(taffy.layout(header_node).size, [800, 100])
+    assert_equal(taffy.layout(body_node).size, [800, 500])
   end
 end
